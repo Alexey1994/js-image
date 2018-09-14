05a863c3bcdab61f1b58d7282d8bd3dcda6933ff
alexey1994 <alexeyborisenko20@gmail.com>
2018-09-14 03:40:33 +0300

    ...

--- main.js
+++ main.js
@@ -11,41 +11,89 @@ window.onresize = function() {
 
 var canvasContext = canvasElement.getContext('2d')
 var image = new Image()
-image.src = './image.jpg'
-
-function calculateImage(image) {
-    var newImage = canvasContext.createImageData(image)
-
-    function getPixel(image, x, y){
-        return image.data[(y * image.width + x) * 4 + 1]
-    }
-
-    function setPixel(image, x, y, pixel){
-        image.data[(y * image.width + x) * 4] = pixel
-        image.data[(y * image.width + x) * 4 + 1] = pixel
-        image.data[(y * image.width + x) * 4 + 2] = pixel
-        image.data[(y * image.width + x) * 4 + 3] = 255
-    }
-
-    for(var y = 0; y < image.height - 1; ++y) {
-        for(var x = 0; x < image.width - 1; ++x) {
-            var top = getPixel(image, x, y)
-            var right = getPixel(image, x + 1, y)
-            var bottom = getPixel(image, x, y + 1)
-
-            if(Math.abs(top - right) > 30 || Math.abs(top - bottom) > 30)
-                setPixel(newImage, x, y, 0)
-        }
-    }
-
-    return newImage
-}
+image.src = 'image.jpg'
 
 image.onload = function() {
     canvasContext.drawImage(image, 0, 0)
-    var imageData = canvasContext.getImageData(0, 0, canvasElement.width, canvasElement.height)
+    var imageSource = canvasContext.getImageData(0, 0, canvasElement.width, canvasElement.height)
 
-    canvasContext.putImageData(calculateImage(imageData), 0, 0)
+    canvasContext.drawImage(image, 0, 0)
+    var imageOutput = canvasContext.getImageData(0, 0, canvasElement.width, canvasElement.height)
+console.log(canvasContext)
+
+    function getPixel(x, y) {
+        return [
+            imageSource.data[(imageSource.width * y + x) * 4],
+            imageSource.data[(imageSource.width * y + x) * 4 + 1],
+            imageSource.data[(imageSource.width * y + x) * 4 + 2]
+        ]
+    }
+
+    function pixelDistance(pixel1, pixel2) {
+        return Math.max(
+            Math.abs(pixel1[0] - pixel2[0]),
+            Math.abs(pixel1[1] - pixel2[1]),
+            Math.abs(pixel1[2] - pixel2[2])
+        )
+    }
+
+    function setPixel(x, y) {
+        imageOutput.data[(imageOutput.width * y + x) * 4] = 255
+        imageOutput.data[(imageOutput.width * y + x) * 4 + 1] = 255
+        imageOutput.data[(imageOutput.width * y + x) * 4 + 2] = 255
+        imageOutput.data[(imageOutput.width * y + x) * 4 + 3] = 255
+    }
+
+    function clearPixel(x, y) {
+        imageOutput.data[(imageOutput.width * y + x) * 4] = 0
+        imageOutput.data[(imageOutput.width * y + x) * 4 + 1] = 0
+        imageOutput.data[(imageOutput.width * y + x) * 4 + 2] = 0
+        imageOutput.data[(imageOutput.width * y + x) * 4 + 3] = 255
+    }
+
+    function clearImage() {
+        for(var y = 0; y < imageOutput.height; ++y)
+            for(var x = 0; x < imageOutput.width; ++x)
+                clearPixel(x, y)
+    }
+
+    clearImage()
+/*
+    for(var y = 0; y < imageSource.height; ++y) {
+        for(var x = 0; x < imageSource.width - 1; ++x) {
+            var currentPixel = getPixel(x, y)
+            var nextPixel = getPixel(x + 1, y)
+
+            if(pixelDistance(currentPixel, nextPixel) > 25) {
+                setPixel(x, y)
+            }
+        }
+    }
+
+    for(var x = 0; x < imageSource.width; ++x) {
+        for(var y = 0; y < imageSource.height - 1; ++y) {
+            var currentPixel = getPixel(x, y)
+            var nextPixel = getPixel(x, y + 1)
+
+            if(pixelDistance(currentPixel, nextPixel) > 25) {
+                setPixel(x, y)
+            }
+        }
+    }*/
+
+    var pattern = [68, 52, 27]//getPixel(0, 0)
+
+    for(var y = 0; y < imageSource.height; ++y) {
+        for(var x = 0; x < imageSource.width; ++x) {
+            var currentPixel = getPixel(x, y)
+
+            if(pixelDistance(currentPixel, pattern) < 20) {
+                setPixel(x, y)
+            }
+        }
+    }
+
+    canvasContext.putImageData(imageOutput, 0, 0)
 }
 
 updateCanvasSize()
